6487eccdb502c62451de3617700678128e919097
lari <lari@kuistio.me>
2020-10-04 20:30:31 +0300

    Modified directory structure Added code to read users from file

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+
+    SECRET_KEY = os.urandom(64)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    WTF_CSRF_ENABLED = True
